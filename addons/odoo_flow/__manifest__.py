f5e9ec3a84aa571dadc185c1b86573db3e6ff0dd
Rafael Silva <rafaelsilva072003@gmail.com>
2026-01-30 10:39:08 +0000

    first commit

---
+++ addons/odoo_flow/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Project Sprints",
+    "version": "1.0.0",
+    "category": "Project",
+    "summary": "Add sprint management to projects and tasks",
+    "depends": ["project"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/sprint_views.xml",
+        "views/project_task_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
